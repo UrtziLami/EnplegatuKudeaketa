1f0514c438d3511cca2fca17c24b4c0969971629
Aitor <45049374+Aitor-ATALAYA@users.noreply.github.com>
2019-10-21 21:12:21 +0200

    loggerra

--- Ethazi1T1/src/kontroladorea/Nagusia.java
+++ Ethazi1T1/src/kontroladorea/Nagusia.java
@@ -1,10 +1,17 @@
 package kontroladorea;
 
+import java.util.logging.Logger;
+
 public class Nagusia {
-
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);//logerraSortu
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+            nireLogerra.setup();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("arasoak log fitxastegiak sortzeko");
+        }
 	}
 
 }
