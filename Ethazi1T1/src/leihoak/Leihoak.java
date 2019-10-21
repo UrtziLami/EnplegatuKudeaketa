823ed3ec39ce19d0dfb38f0903b2333300fd6847
UrtziLami <urtzilamikiz6@gmail.com>
2019-10-21 12:24:31 +0200

    framea

--- Ethazi1T1/src/leihoak/Leihoak.java
+++ Ethazi1T1/src/leihoak/Leihoak.java
@@ -1,12 +1,8 @@
 package leihoak;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.io.IOException;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class Leihoak extends JFrame {
 
